203621e88091ccfb17a6d997d12e665e9b74db07
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-23 02:50:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,35 +6,35 @@
 #   # class TriangleError < StandardError
 #   # end
 # end
-# class Triangle
-#   def initialize(side_1,side_2,side_3)
-#     @side_1 = side_1
-#     @side_2 = side_2
-#     @side_3 = side_3
-#   end
+class Triangle
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
 
-#   def kind()
-#     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-#       raise TriangleError
-#     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-#       raise TriangleError
-#     else
-#       if (@side_1 == @side_2) && (@side_2 == @side_3)
-#         :equilateral
-#       elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-#         :isosceles
-#       elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-#         :scalene
-#       end
-#     end
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
 
-#   end
+  end
 
-# end
+end
 
-# class TriangleError < StandardError
-#   # triangle error code
-# end
+class TriangleError < StandardError
+  # triangle error code
+end
 # class Triangle
 #   attr_accessor :x, :y, :z
 #   def initialize(x, y, z)
